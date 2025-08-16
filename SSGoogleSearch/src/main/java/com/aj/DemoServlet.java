675b04535806d0872cc3a69dabf9ad9b862fac3d
jack690soni <63994895+jack690soni@users.noreply.github.com>
2025-08-16 18:52:25 +0530

    Add files via upload

---
+++ SSGoogleSearch/src/main/java/com/aj/DemoServlet.java
@@ -0,0 +1,40 @@
+package com.aj;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class DemoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public DemoServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		//response.sendRedirect("http://www.google.com");
+        String name=request.getParameter("name");  
+        response.sendRedirect("https://www.google.co.in/search?q=="+name);
+
+		out.close();
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
